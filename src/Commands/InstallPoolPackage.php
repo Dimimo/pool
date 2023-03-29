77b2e84fc294f624ebb911b97bbc1ee9819a01b8
Dimitri Mostrey <dmostrey@yahoo.com>
2023-03-30 00:17:42 +0800

    Pool Commands

---
+++ src/Commands/InstallPoolPackage.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Dimimo\Pool\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+
+class InstallPoolPackage extends Command
+{
+    protected $signature = 'pool:install';
+    protected $description = 'Install the Pool Package';
+
+    public function handle()
+    {
+        $this->info('Installing the Pool Package...');
+        $this->info('Publishing configuration...');
+        if ( ! $this->configExists())
+        {
+            $this->publishConfiguration();
+            $this->info('Published configuration');
+        } else
+        {
+            if ($this->shouldOverwriteConfig())
+            {
+                $this->info('Overwriting configuration file...');
+                $this->publishConfiguration(true);
+            } else
+            {
+                $this->info('Existing configuration was not overwritten');
+            }
+        }
+        $this->info('Installed Pool Package');
+    }
+
+    private function configExists(): bool
+    {
+        return File::exists(config_path('pool.php'));
+    }
+
+    private function publishConfiguration($force = false)
+    {
+        $params = [
+            '--provider' => "Dimimo\Pool\PoolServiceProvider",
+            '--tag'      => "config",
+        ];
+        if ($force === true)
+        {
+            $params['--force'] = true;
+        }
+        $this->call('vendor:publish', $params);
+    }
+
+    private function shouldOverwriteConfig(): bool
+    {
+        return $this->confirm('Config file already exists. Do you want to overwrite it?');
+    }
+}
---
+++ src/Commands/PoolScoresSetDay.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ *  Copyright (c) 2016-2021 Puerto Parrot. Written by Dimitri Mostrey for https// www.puertoparrot.com
+ *  Contact me at admin@puertoparrot.com or dmostrey@yahoo.com
+ */
+
+namespace Dimimo\Pool\Commands;
+
+use Dimimo\Pool\Jobs\PoolSetDayScores;
+use Dimimo\Pool\Models\PoolDate;
+use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
+
+class PoolScoresSetDay extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'pool:scores';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Set the daily pool scores to zero to activate Live Scores';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle(): void
+    {
+        $today = Carbon::now()->format('Y-m-d');
+        $dates = PoolDate::where('date', $today)->get();
+        foreach ($dates as $date)
+        {
+            PoolSetDayScores::dispatch($date);
+        }
+        $dates->count() ? $count = $dates->count() : $count = 'No';
+        $message = "The daily Pool score for $today has been run. $count reset requests has been dispatched.";
+        \Log::info('[PoolScoreSetDay] ' . $message);
+        $this->comment($message);
+    }
+}
